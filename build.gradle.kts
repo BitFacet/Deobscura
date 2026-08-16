5ea6c0e805b0e901bb1f30d507a691cfd1c725af
Johnson <johnssoft@ya.ru>
2026-08-16 14:28:09 +0500

    Initial

---
+++ build.gradle.kts
@@ -0,0 +1,41 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    kotlin("jvm") version "2.3.20"
+}
+
+group = "io.github.relvl"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    jvmToolchain(25)
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_25)
+    }
+}
+
+dependencies {
+    // Kotlin runtime reflection
+    implementation(kotlin("reflect"))
+
+    // Configuration serialization
+    implementation("tools.jackson.module:jackson-module-kotlin:3.2.0")
+
+    // Command line interface
+    implementation("info.picocli:picocli:4.7.7")
+
+    // Logging
+    implementation("org.slf4j:slf4j-api:2.0.18")
+    runtimeOnly("org.slf4j:slf4j-simple:2.0.18")
+
+    // Tests
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
